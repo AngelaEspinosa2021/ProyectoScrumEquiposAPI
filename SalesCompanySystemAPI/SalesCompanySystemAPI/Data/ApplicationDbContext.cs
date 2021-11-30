c77658f056cebbc3b1357b489130a092a9c95349
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-30 17:03:37 -0500

    creación del ApplicationDbContext

---
+++ SalesCompanySystemAPI/SalesCompanySystemAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesCompanySystemAPI.Models;
+
+namespace SalesCompanySystemAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
