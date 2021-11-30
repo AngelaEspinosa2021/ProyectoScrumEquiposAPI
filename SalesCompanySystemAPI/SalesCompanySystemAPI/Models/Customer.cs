11db5f812b98da0371c410d1c424ee3c673ce362
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-30 16:53:33 -0500

    Creacion de la clase Custome con sus respectivas propiedades

---
+++ SalesCompanySystemAPI/SalesCompanySystemAPI/Models/Customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesCompanySystemAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int id_Customer { get; set; }
+
+        [Required]
+        public string customerPhoto { get; set; }
+
+        [Required]
+        public int documentType { get; set; }
+
+        [Required]
+        public string identificationNumber { get; set; }
+
+        [Required]
+        public string nameCustomer { get; set; }
+
+        [Required]
+        public string lastNameCustomer { get; set; }
+
+        [Required]
+        public string addressCustomer { get; set; }
+
+        [Required]
+        public string phoneNumberCustomer { get; set; }
+
+        [Required]
+        public string emailCustomer { get; set; }
+
+        [Required]
+        public string cityCustomer { get; set; }
+
+    }
+}
