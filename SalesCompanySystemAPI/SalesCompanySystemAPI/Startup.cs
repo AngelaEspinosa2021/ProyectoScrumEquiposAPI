f2059557831b0523491337021b6ae370ca22fa38
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-30 18:06:40 -0500

    Instalacion del paquete Swagger con su respectiva configuración.

--- SalesCompanySystemAPI/SalesCompanySystemAPI/Startup.cs
+++ SalesCompanySystemAPI/SalesCompanySystemAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using SalesCompanySystemAPI.Data;
 
 
@@ -30,6 +31,11 @@ namespace SalesCompanySystemAPI
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                         options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
 
             services.AddControllers();
         }
@@ -51,6 +57,13 @@ namespace SalesCompanySystemAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
