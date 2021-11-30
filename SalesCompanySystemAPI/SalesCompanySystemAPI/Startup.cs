e770dc2e9b2f8c04e010295ef009646441565a0a
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-30 17:23:13 -0500

    Referencia al ApplicationDbContext desde ConfigureServices del StartUp

--- SalesCompanySystemAPI/SalesCompanySystemAPI/Startup.cs
+++ SalesCompanySystemAPI/SalesCompanySystemAPI/Startup.cs
@@ -10,6 +10,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SalesCompanySystemAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 
 namespace SalesCompanySystemAPI
 {
@@ -25,6 +29,9 @@ namespace SalesCompanySystemAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
         }
 
