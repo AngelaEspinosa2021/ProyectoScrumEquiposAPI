f08a1160a54e709c8d563e2d2bedd04db8c22116
AngelaEspinosa2021 <angelaespinosa23871@ucaldas.edu.co>
2021-11-30 17:58:03 -0500

    Creación y conexion con SalesCompanySystemDb desde consola

--- SalesCompanySystemAPI/SalesCompanySystemAPI/Data/ApplicationDbContext.cs
+++ SalesCompanySystemAPI/SalesCompanySystemAPI/Data/ApplicationDbContext.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using SalesCompanySystemAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesCompanySystemAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
 
         }
---
+++ SalesCompanySystemAPI/SalesCompanySystemAPI/Migrations/20211130225655_AddSalesCompanySystemDb.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesCompanySystemAPI.Migrations
+{
+    public partial class AddSalesCompanySystemDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    id_Customer = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    customerPhoto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    documentType = table.Column<int>(type: "int", nullable: false),
+                    identificationNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    nameCustomer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    lastNameCustomer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    addressCustomer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    phoneNumberCustomer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    emailCustomer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    cityCustomer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.id_Customer);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
